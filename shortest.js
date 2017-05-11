86d08d1470c1bd187697f8e3a81b8a1fab7899a1
bloom8 <namepr@gmail.com>
2017-05-11 14:35:30 +0800

    Shortest Word
    x Simple, given a string of words, return the length of the shortest
    word(s).
    
    String will never be empty and you do not need to account for different
    data types.

---
+++ shortest.js
@@ -0,0 +1,22 @@
+/*function findShort(string) {
+  var array = string.split(' ');
+  var shortest = null;
+  
+  for (var i=0, l=array.length; i<l; ++i)
+  {
+    if (shortest == null || array[i].length < shortest)
+      shortest = array[i].length;
+  }
+  
+  return shortest;
+}*/
+
+function findShort(s){
+var arr=s.split("");
+var ss=arr[0];
+for (var i=0;i<arr.lenght;i++){
+          if(arr[i].length<ss.length)
+          {ss=arr[i];}
+          }
+  return ss.length;        
+}
